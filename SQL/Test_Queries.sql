e0e547deb8a0a4b6908bb1af4fb98c8ef82080e0
Ling Balmforth <Ling.Balmforth@softwire.com>
2022-03-01 19:45:42 +0000

    Fixed Test_Queries.sql

--- SQL/Test_Queries.sql
+++ SQL/Test_Queries.sql
@@ -16,7 +16,13 @@ FROM Book as b
 JOIN Book_Author AS ba ON b.Book_ID = ba.Book_ID 
 JOIN Author AS a ON a.Author_ID = ba.Author_ID
 JOIN Book_Item as bi ON bi.Book_ID = b.Book_Id 
-JOIN Loan as l ON l.book_item_id = l.book_item_id 
+JOIN Loan as l ON bi.book_item_id = l.book_item_id 
 JOIN member as m on l.member_id = m.member_id
 JOIN address as ad on ad.address_id = m.address_id 
+WHERE l.date_returned is null;
+
+
+SELECT  bi.book_item_id, l.loan_id, bi.book_id
+FROM Book_Item as bi 
+JOIN Loan as l ON bi.book_item_id = l.book_item_id 
 WHERE l.date_returned is null;
